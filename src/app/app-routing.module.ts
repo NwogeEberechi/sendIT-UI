03382fcaea16b9955d3f83a1f0d3db79c3fdec72
NwogeEberechi <eberechinwoge@gmail.com>
2018-12-04 17:27:16 +0100

    feature(login):create login page - generate login component - add login route - add login form - style elements
    [starts #162411682]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/accounts/login/login.component';
 
 const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
   {
     path: '',
     component: HomeComponent
